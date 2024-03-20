ab94af843131653367858c124c855d4c2729b941
Astro <James@sightsource.net>
2024-03-20 11:04:43 -0400

    Initial commit

---
+++ __brick__/{{name.snakeCase()}}/presentation/presentation.dart
@@ -0,0 +1,3 @@
+export 'widgets/widgets.dart';
+export 'screens/screens.dart';
+export 'controllers/controllers.dart';
---
+++ __brick__/{{name.snakeCase()}}/{{name.snakeCase()}}.dart
@@ -0,0 +1,4 @@
+export 'presentation/presentation.dart';
+export 'domain/domain.dart';
+export 'application/application.dart';
+export 'data/data.dart';
